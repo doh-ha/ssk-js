e570e6c469690a2e6ab42adbbe014ef0d80108ea
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-06-29 15:19:51 +0900

    ✨ 로컬 폰트 설정
    font-family를 통해 사용 가능

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Font from "expo-font";
 
 import RootNavigator from "./src/navigators/RootNavigator";
 
 export default function App () {
+
+  Font.loadAsync({
+    Light: require("./src/global/fonts/NanumSquareL.ttf"),
+    Regular: require("./src/global/fonts/NanumSquareR.ttf"),
+    Medium: require("./src/global/fonts/NanumSquare.ttf"),
+    Bold: require("./src/global/fonts/NanumSquareB.ttf"),
+    ExtraBold: require("./src/global/fonts/NanumSquareEB.ttf"),
+  });
 
   return (
     <>
--- src/screens/root/OnBoardingScreen.jsx
+++ src/screens/root/OnBoardingScreen.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+import Layout from "../../components/common/Layout";
+import PageWrapper from "../../components/common/PageWrapper";
+import Header from "../../components/common/Header";
+
 const OnBoardingScreen = () => {
 
-  
+
   return (
-    <>
-    
-    </>
+    <Layout>
+      <PageWrapper>
+        <Header text="온보딩 화면" type="basic"/>
+      </PageWrapper>
+    </Layout>
   );
 };
 
