086375a90c67ae516c43839e8186da0fc6ef9e9d
Ahyoung-Kim <eileel@naver.com>
2023-06-30 14:09:10 +0900

    :lipstick: Tab navigator UI update
    탭네비게이터 UI 수정

---
+++ src/common/color.js
@@ -0,0 +1,8 @@
+export default {
+  COLOR_MAIN: "#0C9BFB",
+
+  COLOR_TAB_ICON: "#D2D2D2",
+
+  COLOR_WHITE_BACKGROUND: "#ffffff",
+  COLOR_BOX_SHADOW: "#555555",
+};
--- src/navigators/TabNavigator.jsx
+++ src/navigators/TabNavigator.jsx
@@ -1,63 +1,73 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome } from "@expo/vector-icons";
+import { Entypo, FontAwesome5 } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/tab/HomeScreen";
 import ClassListScreen from "../screens/tab/ClassListScreen";
 import MyPageScreen from "../screens/tab/MyPageScreen";
+import color from "../common/color";
 
 const TabBar = createBottomTabNavigator();
 
 const TabNavigator = () => {
-
   return (
-    <TabBar.Navigator>
-      <TabBar.Screen
-        name="홈"
-        component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarLabelStyle: {
-            color: "#808080"
+    <TabBar.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: color.COLOR_WHITE_BACKGROUND,
+          shadowColor: color.COLOR_BOX_SHADOW,
+          shadowOffset: {
+            width: 0,
+            height: -3,
           },
-          tabBarIcon: ({ focused }) =>
-            !focused  ? (
-              <FontAwesome name="address-book" size={24} color="#e0e0e0" />
-              ) : (
-              <FontAwesome name="address-book" size={24} color="#0c9bfb" />
-            ),
-        }}
-      />
+          shadowOpacity: 0.2,
+        },
+      }}
+    >
       <TabBar.Screen
-        name="수업 목록"
+        name="ClassList"
         component={ClassListScreen}
         options={{
           headerShown: false,
-          tabBarLabelStyle: {
-            color: "#808080"
-          },
-          tabBarIcon: ({ focused }) =>
-            !focused  ? (
-              <FontAwesome name="list-ul" size={24} color="#e0e0e0" />
-              ) : (
-              <FontAwesome name="list-ul" size={24} color="#0c9bfb" />
-            ),
+          tabBarLabel: "수업 목록",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="list-ul"
+              size={25}
+              color={focused ? color.COLOR_MAIN : color.COLOR_TAB_ICON}
+            />
+          ),
         }}
       />
       <TabBar.Screen
-        name="마이 페이지"
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "홈",
+          tabBarIcon: ({ focused }) => (
+            <Entypo
+              name="home"
+              size={30}
+              color={focused ? color.COLOR_MAIN : color.COLOR_TAB_ICON}
+            />
+          ),
+        }}
+      />
+      <TabBar.Screen
+        name="MyPage"
         component={MyPageScreen}
         options={{
           headerShown: false,
-          tabBarLabelStyle: {
-            color: "#808080"
-          },
-          tabBarIcon: ({ focused }) =>
-            !focused  ? (
-              <FontAwesome name="user-circle" size={24} color="#e0e0e0" />
-              ) : (
-              <FontAwesome name="user-circle" size={24} color="#0c9bfb" />
-            ),
+          tabBarLabel: "마이 페이지",
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5
+              name="user-alt"
+              size={25}
+              color={focused ? color.COLOR_MAIN : color.COLOR_TAB_ICON}
+            />
+          ),
         }}
       />
     </TabBar.Navigator>
