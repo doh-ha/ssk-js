2df23f9c3396e85745712c6c088034bb87482097
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-07-04 15:32:13 +0900

    💄 헤더

--- src/screens/tab/ClassListScreen.jsx
+++ src/screens/tab/ClassListScreen.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import MainLayout from "../../components/common/MainLayout";
+import { Text } from "react-native";
 
 const ClassListScreen = () => {
   return (
     <>
+      <MainLayout headerText={"선생님 목록"} headerType={"basic"}>
+        <Text>선생님목록</Text>
+      </MainLayout>
     </>
   );
 };
