b0271aff76a9f4a5e730a6c9cd72fcb3cb7b841b
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-07-06 17:05:13 +0900

    💄ClassInfo에 노트 바로가기 레이아웃

--- src/screens/ClassInfo/ClassInfoScreen.jsx
+++ src/screens/ClassInfo/ClassInfoScreen.jsx
@@ -3,16 +3,22 @@ import React from "react";
 import MainLayout from "../../components/common/MainLayout";
 import styled from "styled-components/native";
 import Calendar from "../../components/calendar/Calendar";
+import HwNoteBox from "../../components/homeworkNote/HwNoteBox";
+import { FlatList, ScrollView } from "react-native";
+import { Container } from "postcss";
 
 const ClassInfoScreen = () => {
   return (
     <>
-      <MainLayout headerText={"수업 정보"} headerType={"basic"}>
-        <Calendar />
-      </MainLayout>
+      <ScrollView>
+        <MainLayout headerText={"수업 정보"} headerType={"basic"}>
+          <Calendar />
+          <HwNoteBox />
+          <HwNoteBox />
+        </MainLayout>
+      </ScrollView>
     </>
   );
 };
 
 export default ClassInfoScreen;
-const Container = styled.FlatList``;
