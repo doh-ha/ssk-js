369023781bf3bfef1fc901d5f1f841fa1bf71b92
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-07-06 14:25:04 +0900

    💄 수업 목록

--- src/screens/tab/ClassListScreen.jsx
+++ src/screens/tab/ClassListScreen.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import MainLayout from "../../components/common/MainLayout";
-import { Text } from "react-native";
+import ProfileImage from "../../components/common/ProfileImage";
+import styled from "styled-components/native";
+import ClassContainer from "../../components/classList/ClassContainer";
+import ClassItem from "../../components/common/ClassItem";
 
 const ClassListScreen = () => {
   return (
     <>
-      <MainLayout headerText={"선생님 목록"} headerType={"basic"}>
-        <Text>선생님목록</Text>
+      <MainLayout headerText={"수업 목록"} headerType={"basic"}>
+        <Container data={[0, 1]} renderItem={ClassItem} />
       </MainLayout>
     </>
   );
 };
 
 export default ClassListScreen;
+const Container = styled.FlatList`
+  width: 100%;
+  overflow: auto;
+`;
