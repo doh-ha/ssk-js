1dc7874dac9c29f1a7c67d4b7aee9648e659e7dc
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-07-07 12:35:45 +0900

    ✨ 키보드 노출 시 로고 없애기

--- src/navigators/RootNavigator.jsx
+++ src/navigators/RootNavigator.jsx
@@ -9,7 +9,7 @@ import TabNavigator from "./TabNavigator";
 const Stack = createNativeStackNavigator(); // Stack 네비게이터 생성
 const RootNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="TabNavigator">
+    <Stack.Navigator initialRouteName="LoginScreen">
       <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }} />
       <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} options={{ headerShown: false }} />
--- src/screens/root/LoginScreen.jsx
+++ src/screens/root/LoginScreen.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import Toast from "react-native-toast-message";
+import { Keyboard } from "react-native";
 
 import Layout from "../../components/common/Layout";
 import PageWrapper from "../../components/common/PageWrapper";
@@ -10,8 +11,30 @@ import LoginForm from "../../components/login/LoginForm";
 import LoginOptions from "../../components/login/LoginOptions";
 
 const LoginScreen = () => {
+  const [isKeyboardShown, setIsKeyboardShown] = useState(false);
   const successMessage = () => Toast.show({type: "success", text1: "성공적으로 로그인했습니다!"});
   const errorMessage = () => Toast.show({type: "error", text1: "아이디나 비밀번호를 확인해주세요!"});
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow",
+      () => {
+        setIsKeyboardShown(true);
+      }
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      Platform.OS === "android" ? "keyboardDidHide" : "keyboardWillHide",
+      () => {
+        setIsKeyboardShown(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
 
   return (
     <>
@@ -22,9 +45,14 @@ const LoginScreen = () => {
             topOffset={20}
             visibilityTime={1200}
           />
-          <Margin size={130}/>
-          <LoginLogo />
-          <Margin size={40}/>
+          <Margin size={90} />
+          {!isKeyboardShown && (
+            <>
+              <Margin size={40} />
+              <LoginLogo />
+              <Margin size={40} />
+            </>
+          )}
           <LoginForm successMessage={successMessage} errorMessage={errorMessage} />
           <LoginOptions />
         </PageWrapper>
