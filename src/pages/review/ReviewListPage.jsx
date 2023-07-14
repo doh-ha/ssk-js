19c8fc6ff721048263917e7469accd009d7b1e23
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-07-14 17:15:42 +0900

    ✨복습노트 진행중

--- src/pages/review/ReviewListPage.jsx
+++ src/pages/review/ReviewListPage.jsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import MainLayout from "../../components/common/MainLayout";
 import ContinuingReview from "../../components/reviewNote/ContinuingReview";
 import color from "../../common/color";
 import { Ionicons } from "@expo/vector-icons";
 import FinishedReview from "../../components/reviewNote/FinishedReview";
+import KeyboardAvoidingLayout from "../../components/common/KeyboardAvoidingLayout";
+import TextInputForm from "../../components/inputs/TextInputForm";
 const ReviewListPage = () => {
+  const initialState = {
+    id: 0,
+    title: "",
+    discription: "",
+    completed: false,
+  };
+  const [newTodo, setNewTodo] = useState(initialState);
+  const [todo, setTodo] = useState([]);
   return (
-    <MainLayout headerText={"복습 노트"} headerType={"back"}>
-      <InnerWrapper>
-        <TopWrapper>
-          <Ionicons name="create-outline" size={32} color="#0000" />
-          <BoldText>복습 목록</BoldText>
-          <Ionicons name="trash-outline" size={32} color="#B0B0B0" />
-        </TopWrapper>
+    <KeyboardAvoidingLayout>
+      <MainLayout headerText={"복습 노트"} headerType={"back"}>
+        <InnerWrapper>
+          <TopWrapper>
+            <Ionicons name="create-outline" size={32} color="#0000" />
+            <BoldText>복습 목록</BoldText>
+            <Ionicons name="trash-outline" size={32} color="#B0B0B0" />
+          </TopWrapper>
 
-        <ContinuingReview />
-        <FinishedReview />
-      </InnerWrapper>
-    </MainLayout>
+          <ContinuingReview />
+          <FinishedReview />
+        </InnerWrapper>
+        <TextInputForm placeholder={"복습 내용을 입력하세요."} />
+      </MainLayout>
+    </KeyboardAvoidingLayout>
   );
 };
 
